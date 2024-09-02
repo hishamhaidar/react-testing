f1e9a8459b7b0f4d252b76ee7f02b0c23e96d463
Hisham Haidar <hhaidar@isa.ae>
2024-09-02 07:58:50 +0300

    user list testing

---
+++ src/tests/UserList.test.js
@@ -0,0 +1,31 @@
+import { render ,screen, within } from "@testing-library/react";
+import UserList from "../UserList";
+
+test('render one row per user',async () => {
+const usersMock = [
+    {name:'h',email:'s'},
+    {name:'hd',email:'ss'},
+]
+render(<UserList users={usersMock}/>)
+const rows = within(screen.getByTestId('users')).getAllByRole('row'); // NOT best idea always
+// const {container} =render(<UserList users={usersMock}/>)
+// // eslint-disable-next-line
+// const rows = container.querySelectorAll('tbody tr')
+
+expect(rows).toHaveLength(2)
+
+})
+
+test('rendering users',async () => {
+    const usersMock = [
+        {name:'h',email:'s'},
+        {name:'hd',email:'ss'},
+    ]
+    render(<UserList users={usersMock}/>)
+    for (let user of usersMock){
+        const name =  screen.getByRole('cell' , {name : user.name})
+        const email = screen.getByRole('cell' , {name : user.email})
+
+        expect(name).toBeInTheDocument()
+        expect(email).toBeInTheDocument()
+    })
