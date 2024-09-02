f7d2d4ec21ed0d2418e85d2ba008bfa9afadaf04
Hisham Haidar <hhaidar@isa.ae>
2024-09-02 08:16:12 +0300

    avoid redundancy

--- src/tests/UserList.test.js
+++ src/tests/UserList.test.js
@@ -1,12 +1,17 @@
 import { render ,screen, within } from "@testing-library/react";
 import UserList from "../UserList";
 
+const renderComponent = () =>{
+    const usersMock = [
+        {name:'h',email:'s'},
+        {name:'hd',email:'ss'},
+    ]
+    render(<UserList users={usersMock}/>)
+    return {usersMock};
+}
+
 test('render one row per user',async () => {
-const usersMock = [
-    {name:'h',email:'s'},
-    {name:'hd',email:'ss'},
-]
-render(<UserList users={usersMock}/>)
+renderComponent()
 const rows = within(screen.getByTestId('users')).getAllByRole('row'); // NOT best idea always
 // const {container} =render(<UserList users={usersMock}/>)
 // // eslint-disable-next-line
@@ -17,15 +22,12 @@ expect(rows).toHaveLength(2)
 })
 
 test('rendering users',async () => {
-    const usersMock = [
-        {name:'h',email:'s'},
-        {name:'hd',email:'ss'},
-    ]
-    render(<UserList users={usersMock}/>)
+    const {usersMock} = renderComponent();
+
     for (let user of usersMock){
         const name =  screen.getByRole('cell' , {name : user.name})
         const email = screen.getByRole('cell' , {name : user.email})
 
         expect(name).toBeInTheDocument()
         expect(email).toBeInTheDocument()
-    })
+    }})
